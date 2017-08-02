7e41c0a5caab626639bf9bc5e799b46935f02526
Bapexboz <Bapexboz@users.noreply.github.com>
2017-08-02 23:57:23 +0800

    0802-Leo_update2
    delete SQL

--- 支付用户回访情况分析.sql
+++ 支付用户回访情况分析.sql
@@ -4,22 +4,22 @@ from
 from 
 (select distinct datekey datekey, user_id
 from ba_travel.fact_mt_sale_platform_domestic_new_pay_user
-where datekey between 20170706 and 20170719)y
+where datekey between 20170701 and 20170731)y
 
 left join 
 
 
 (select distinct a.datekey datekey, c.user_id
 from ba_travel.log_domestic_intention_mt_new_uuid a
-join ba_travel.topic_order_date c on a.uuid=c.uuid and c.datekey between 20170706 and 20170719
-and a.datekey between 20170706 and 20170719 )x  on x.user_id = y.user_id 
+join ba_travel.topic_order_date c on a.uuid=c.uuid and c.datekey between 20170701 and 20170731
+and a.datekey between 20170701 and 20170731 )x  on x.user_id = y.user_id 
 where x.datekey>=y.datekey
 group by  x.datekey,y.datekey  ) aa   
 
 join 
 (select datekey,count(distinct user_id) pay_user_num
 from ba_travel.fact_mt_sale_platform_domestic_new_pay_user
-where datekey between 20170706 and 20170719
+where datekey between 20170701 and 20170731
 group by datekey)bb 
 
 on aa.datekey1=bb.datekey
