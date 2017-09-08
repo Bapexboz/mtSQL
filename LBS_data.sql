5c65ce2f0a5dcddf256e525b61063cd4e4c496cb
Bapexboz <Bapexboz@users.noreply.github.com>
2017-09-09 07:51:33 +0800

    0909-Leo-update_1
    modify SQL

--- LBS_data.sql
+++ LBS_data.sql
@@ -10,5 +10,5 @@ select
     order_uv,
     order_gmv
 from mart_semantic.aggr_realtime_delivery_daily
-where partition_date ='2017-08-21' and '2017-08-27'
+where partition_date  between '$begindatekey' and '$enddatekey'
 and range='current'
